a43f60aed1ed783329867d3de0e503a239f1507f
BillLynch <llynch.bill@gmail.com>
2019-03-20 00:38:50 +0000

    some more comments

--- polarisingfilter.cpp
+++ polarisingfilter.cpp
@@ -1,8 +1,18 @@
 #include <utility>
 
 #include "polarisingfilter.h"
-#include <iostream>
 
+/*!
+ * \brief PolarisingFilter::PolarisingFilter
+ * \param location
+ * \param side
+ * \param normal
+ * \param radius
+ * \param n1
+ * \param targetPolarisation
+ *
+ * Constructor for the polarisingFilter, also creates the matrix for the Jones Calculus.
+ */
 PolarisingFilter::PolarisingFilter(Eigen::Vector3d location, int side,
                                    Eigen::Vector3d normal, double radius,
                                    std::complex<double> n1,
@@ -14,6 +24,17 @@ PolarisingFilter::PolarisingFilter(Eigen::Vector3d location, int side,
   calculatePolarisationMatrix();
 }
 
+/*!
+ * \brief PolarisingFilter::collide
+ * \param ray
+ * \param pointOfInterception
+ *
+ * This method is called on clission with a ray.
+ * If the polarisation has changed we tell the hysteresis loop graph.
+ * We calculate the Jones Calculus matrix again (incase noise is included), then
+ * reposition the ray infront of the filter. We Trigger the 3D view's idea of the
+ * rays to update based on the latest ray which has gone though the filter.
+ */
 void PolarisingFilter::collide(Ray &ray, Eigen::Vector3d &pointOfInterception) {
   auto tempEr = Eigen::Vector2cd(ray.getPolarisation()(0, 0),
                                  ray.getPolarisation()(1, 1));
@@ -33,6 +54,16 @@ void PolarisingFilter::collide(Ray &ray, Eigen::Vector3d &pointOfInterception) {
   emit outputPolarisationUpdated(ray.getPolarisation());
 }
 
+/*!
+ * \brief PolarisingFilter::intersect
+ * \param ray
+ * \param pointOfInterception
+ * \return if there was a collision with the filter and this ray.
+ *
+ * Check if we collide with the plane of infinate width and hight at the
+ * position of the filter. We then detect if the collision point was within the
+ * radiius of the filter. Very similar to the implementation in the PEM.
+ */
 bool PolarisingFilter::intersect(Ray &ray,
                                  Eigen::Vector3d &pointOfInterception) {
   double t;
